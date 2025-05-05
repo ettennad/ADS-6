3413befc056364d0f782261a3e99f713a1640099
ettennad <a.derlukova@mail.ru>
2025-05-05 21:37:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,67 +1,51 @@
 // Copyright 2021 NNTU-CS
-#include <stdexcept>
-
 template<typename T>
 class TPQueue {
  private:
   struct Node {
-    T data;
-    Node* next;
-    explicit Node(const T& value, Node* next = nullptr)
-      : data(value), next(next) {}
+      T data;
+      Node* next;
+      explicit Node(const T& value) : data(value), next(nullptr) {}
   };
   Node* head;
-  Node* tail;
-
  public:
-  TPQueue() : head(nullptr), tail(nullptr) {}
+  TPQueue() : head(nullptr) {}
   ~TPQueue() {
-    while (head) {
+      while (head) {
+          Node* temp = head;
+          head = head->next;
+          delete temp;
+      }
+  }
+  void push(const T& value) {
+      Node* newNode = new Node(value);
+      if (!head || value.prior > head->data.prior) {
+          newNode->next = head;
+          head = newNode;
+          return;
+      }
+      Node* current = head;
+      while (current->next && current->next->data.prior >= value.prior) {
+          current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
+  }
+  T pop() {
+      if (!head) {
+          throw "Queue is empty";
+      }
       Node* temp = head;
+      T value = head->data;
       head = head->next;
       delete temp;
-    }
+      return value;
   }
-
-  void push(const T& value) {
-    Node* newNode = new Node(value);
-    if (!head || item.prior > head->data.prior) {
-      newNode->next = head;
-      head = newNode;
-      if (!tail) tail = head;
-      return;
-    }
-   Node* current = head;
-   while (current->next && 
-    current->next->data.prior >= value.prior) {
-    current = current->next;
-   }
-   newNode->next = current->next;
-   current->next = newNode;
-   if (!newNode->next) {
-    tail = newNode;
-   }
+  bool isEmpty() const {
+      return head == nullptr;
   }
-
-T pop() {
- if (!head) {
-  throw std::runtime_error("Queue is empty");
- }
- Node* temp = head;
- T value = head->data;
- head = head->next;
- if (!head) {
-  tail = nullptr;
- }
- delete temp;
- return value;
-}
-bool isEmpty() const {
- return head == nullptr;
-}
 };
-
 struct SYM {
-char ch;
-int prior;
+  char ch;
+  int prior;
 };
