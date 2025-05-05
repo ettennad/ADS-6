6bf0f1ccbbb41d31d5fe9a0bd26370573a92822c
ettennad <a.derlukova@mail.ru>
2025-05-05 21:51:14 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,59 +4,59 @@
 
 template<typename T>
 class TPQueue {
-private:
-    struct Node {
-        T data;
-        Node* next;
-        explicit Node(const T& value) : data(value), next(nullptr) {}
-    };
+ private:
+  struct Node {
+    T data;
+    Node* next;
+    explicit Node(const T& value) : data(value), next(nullptr) {}
+  };
+  
+  Node* head;
+
+ public:
+  TPQueue() : head(nullptr) {}
+  
+  ~TPQueue() {
+    while (head) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+
+  void push(const T& value) {
+    Node* newNode = new Node(value);
     
-    Node* head;
-
-public:
-    TPQueue() : head(nullptr) {}
+    if (!head || value.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+      return;
+    }
     
-    ~TPQueue() {
-        while (head) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
+    Node* current = head;
+    while (current->next && current->next->data.prior >= value.prior) {
+      current = current->next;
     }
+    
+    newNode->next = current->next;
+    current->next = newNode;
+  }
 
-    void push(const T& value) {
-        Node* newNode = new Node(value);
-        
-        if (!head || value.prior > head->data.prior) {
-            newNode->next = head;
-            head = newNode;
-            return;
-        }
-        
-        Node* current = head;
-        while (current->next && current->next->data.prior >= value.prior) {
-            current = current->next;
-        }
-        
-        newNode->next = current->next;
-        current->next = newNode;
+  T pop() {
+    if (!head) {
+      throw "Queue is empty";
     }
+    
+    Node* temp = head;
+    T value = head->data;
+    head = head->next;
+    delete temp;
+    return value;
+  }
 
-    T pop() {
-        if (!head) {
-            throw "Queue is empty";
-        }
-        
-        Node* temp = head;
-        T value = head->data;
-        head = head->next;
-        delete temp;
-        return value;
-    }
-
-    bool isEmpty() const {
-        return head == nullptr;
-    }
+  bool isEmpty() const {
+    return head == nullptr;
+  }
 };
 
 struct SYM {
